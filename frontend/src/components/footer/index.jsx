743dbd62ba7215b0e009efaafa0b12661efa808d
hamza benidar <benidar.hamza@gmail.com>
2022-05-12 17:29:32 +0200

    start footer

---
+++ frontend/src/components/footer/index.jsx
@@ -0,0 +1,12 @@
+import SFooter from "./style";
+
+function Footer() {
+  return (
+    <SFooter>
+      <p>Mentions légales</p>
+      <p>Nous contacter</p>
+      <p>Team</p>
+    </SFooter>
+  );
+}
+export default Footer;
