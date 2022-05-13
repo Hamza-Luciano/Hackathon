a4c1f6753d7d77f6ec254be5475e14a54498f85e
Decambray Samir <92584130+SDecambray@users.noreply.github.com>
2022-05-13 03:27:24 +0200

    do card with API (#17)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Association from "./components/Association";
 import Home from "./pages/Home";
 import "./App.css";
 import Footer from "./components/Footer";
+import CardForm from "./components/CardForm";
+import Association from "./components/Association";
+
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/association" element={<Association />} />
+        <Route path="/mycity" element={<CardForm />} />
       </Routes>
       <Footer />
     </div>
