723ea07694170b22e6d971882bc45bdc8121a328
NtGZeko <51087692+NtGZeko@users.noreply.github.com>
2022-05-13 10:58:52 +0200

    Fix nav (#30)
    * fix width
    
    * fix navbar
    
    * fix navbar
    
    Co-authored-by: Decambray Samir <92584130+SDecambray@users.noreply.github.com>

--- frontend/src/components/Nav/style.jsx
+++ frontend/src/components/Nav/style.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export default styled.nav`
-  background-color: black;
+
+  background-color: #c2f189;
+
   ul {
     list-style: none;
     padding: 0;
@@ -11,17 +13,15 @@ export default styled.nav`
     text-decoration: none;
   }
   .navBar {
-    background-color: transparent;
+ 
     color: #fff;
-    position: fixed;
-    width: 100vw;
+
     min-height: 75px;
     display: flex;
     justify-content: center;
     align-items: center;
   }
   .navBarSolid {
-    background-color: #0000009d;
     color: #fff;
     transition: 0.3s;
   }
