36f98568e9f095640bac74c79446c46ba53c830a
SDecambray <samir.decambray@gmail.com>
2022-05-13 03:26:18 +0200

    do card with API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
 import Footer from "./components/Footer";
-
+import CardForm from "./components/CardForm";
 import Association from "./components/Association";
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/association" element={<Association />} />
+        <Route path="/mycity" element={<CardForm />} />
       </Routes>
-
       <Footer />
     </div>
   );
