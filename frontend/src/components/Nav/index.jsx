ead54e637b7a49e033536c69abb5d134253b736a
SDecambray <samir.decambray@gmail.com>
2022-05-13 11:58:58 +0200

    caca

--- frontend/src/components/Nav/index.jsx
+++ frontend/src/components/Nav/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import SNav from "./style";
 
 export default function Header() {
@@ -18,18 +19,20 @@ export default function Header() {
 
   return (
     <SNav>
-      <nav
-        className={`navBar ${showLinks ? "showNav" : "hideNav"} ${
-          y === 0 ? "navBar" : "navBarSolid"
-        }`}
-      >
-        <img
-          className="navBarLogo"
-          src="src/assets/logo.png"
-          alt="logo cinerama"
-        />
-        <h1>Change Your World</h1>
-      </nav>
+      <Link to="/">
+        <nav
+          className={`navBar ${showLinks ? "showNav" : "hideNav"} ${
+            y === 0 ? "navBar" : "navBarSolid"
+          }`}
+        >
+          <img
+            className="navBarLogo"
+            src="src/assets/logo.png"
+            alt="logo cinerama"
+          />
+          <h1>Change Your World</h1>
+        </nav>
+      </Link>
     </SNav>
   );
 }
