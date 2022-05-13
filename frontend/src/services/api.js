21b6544d19e7311f903889579e1a059657027e21
Amar <amar@uub.localdomain>
2022-05-13 04:27:43 +0200

    files-add

---
+++ frontend/src/services/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://beta3.api.climatiq.io/",
+});
+api.defaults.headers.authorization = "Bearer CSZHDB0CZ4MWK3JF8GYN7TAGWRQ1";
+
+export default api;
