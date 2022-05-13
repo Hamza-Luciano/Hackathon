9fc6a83e68eec28d2c83496abdac0e2571297faf
hamza benidar <benidar.hamza@gmail.com>
2022-05-13 11:35:01 +0200

    minor fix

--- frontend/src/components/Association/index.jsx
+++ frontend/src/components/Association/index.jsx
@@ -15,7 +15,7 @@ import SAssociation from "./style";
 export default function Association() {
   return (
     <SAssociation>
-      <h1> Donnez pour aider </h1>
+      <h1 className="titre"> Donner pour aider </h1>
       <p>
         L’environnement est une richesse précieuse qu’il convient de préserver
         pour son développement durable. En effet, l’homme tire l’essentiel de
@@ -62,7 +62,7 @@ export default function Association() {
       </a>
       <h3>
         <a className="title" href="https://www.fnh.org/">
-          <img src={FNH} alt="FNH logo" className="logo" />
+          <img src={FNH} alt="FNH logo" className="logo logo2" />
         </a>
       </h3>
       <p className="text">
@@ -134,11 +134,7 @@ export default function Association() {
       </a>
       <h3>
         <a className="title" href="https://www.associationhirondelle.fr/">
-          <img
-            src={logoHIRONDELLE}
-            alt="Hirondelle logo"
-            className="logo wwf"
-          />
+          <img src={logoHIRONDELLE} alt="Hirondelle logo" className="logo " />
         </a>
       </h3>
       <p className="text">
@@ -159,10 +155,9 @@ export default function Association() {
       >
         Faire un don
       </a>
-      <img src={ImgAsso1} alt="Animal" />
       <h3>
         <a className="title" href="https://www.wwf.fr/">
-          <img src={WWFLogo} alt="wwf logo" className="logo wwf" />
+          <img src={WWFLogo} alt="wwf logo" className="logo " />
         </a>
       </h3>
 
