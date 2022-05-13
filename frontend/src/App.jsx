71071d20b5f2ee5efde5ec7a5b77fe3812cbde52
Amar <amar@uub.localdomain>
2022-05-13 04:00:05 +0200

    motor-type

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
+import VehiclesEmissions from "@components/VehiclesEmissions";
 import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <VehiclesEmissions />
     </div>
   );
 }
